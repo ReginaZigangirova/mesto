764897fa71c2e9713914a59eb8b1c6660d9cd42c
Regina Zigangirova <zigangirova.reggina@yandex.ru>
2022-03-03 12:25:37 +0300

    Место 5.1

--- scripts/PopupWithImage.js
+++ scripts/PopupWithImage.js
@@ -3,14 +3,14 @@ import Popup from './Popup.js';
 export default class PopupWithImage extends Popup {
     constructor(popupSelector) {
         super(popupSelector);
-        this._popupPicture = document.querySelector('.popup__img');
+        this.popupImg = document.querySelector('.popup__img');
         this._popupDescription = document.querySelector('.popup__title');
     }
 
     open(name, link) {
         this._popupDescription.textContent = name;
-        this._popupPicture.src = link;
-        this._popupPicture.alt = name;
+        this.popupImg.src = link;
+        this.popupImg.alt = name;
         super.open();
     }
 }
