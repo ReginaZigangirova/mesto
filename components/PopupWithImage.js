1bd5251ee4b9e48cd18aa996c2cfcdb6c99d1bd9
Regina Zigangirova <zigangirova.reggina@yandex.ru>
2022-03-04 12:46:27 +0300

    Место 5.2

--- components/PopupWithImage.js
+++ components/PopupWithImage.js
@@ -3,14 +3,14 @@ import Popup from './Popup.js';
 export default class PopupWithImage extends Popup {
     constructor(popupSelector) {
         super(popupSelector);
-        this.popupImg = document.querySelector('.popup__img');
+        this._popupImg = document.querySelector('.popup__img');
         this._popupDescription = document.querySelector('.popup__title');
     }
 
     open(name, link) {
         this._popupDescription.textContent = name;
-        this.popupImg.src = link;
-        this.popupImg.alt = name;
+        this._popupImg.src = link;
+        this._popupImg.alt = name;
         super.open();
     }
 }
--- utils/constants.js
+++ utils/constants.js
@@ -24,6 +24,7 @@ export const initialCards = [{
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
+
 export const validationConfig = {
     formSelector: '.popup__form',
     inputSelector: '.popup__input',
@@ -33,30 +34,20 @@ export const validationConfig = {
     errorClass: '.popup__input-error_active'
 };
 //модалка профиля
-const popups = document.querySelectorAll('.popup');
 const editModal = document.querySelector('.popup_type_edit');
 export const editOpenButton = document.querySelector('.profile__button');
-const editCloseButton = editModal.querySelector('.popup__close');
 
 export const editForm = editModal.querySelector('.popup__form');
-const placeInput = editForm.querySelector('.popup__input_type_name');
-const jobInput = editForm.querySelector('.popup__input_type_job');
 
 //модалка добавления карточки 
 export const addCardModal = document.querySelector('.popup_type_add-card');
 export const addCardButton = document.querySelector('.profile__button-plus');
-const addCardCloseButton = addCardModal.querySelector('.popup__close');
 export const addCardForm = addCardModal.querySelector('.popup__form');
 
 //модалка картинки
-export const imgModal = document.querySelector('.popup_type_img');
-const imgModalCloseButton = imgModal.querySelector('.popup__close');
-export const modalImg = document.querySelector('.popup__img');
-export const modaltitle = document.querySelector('.popup__title');
+//const imgModalCloseButton = imgModal.querySelector('.popup__close');
 
 export const section = document.querySelector('.card');
-const inputCardName = document.querySelector('.popup__input_type_card-name');
-const inputCardLink = document.querySelector('.popup__input_type_card-link');
 
 export const editSelector = {
     profileName: '.profile__name',
