8107a3a7ec58452fdf2096aff86dd228877022b1
Regina Zigangirova <zigangirova.reggina@yandex.ru>
2022-03-20 15:58:05 +0300

    Место 6.1

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,23 +9,40 @@ import { editSelector, editOpenButton, editForm, addCardForm, addCardButton, sec
 import { api } from '../components/Api.js';
 import './index.css';
 
+// Promise.all([api.getProfile(), api.getInitialCards()])
+//     .then(([res, cardList]) => {
+//         userInfo.setUserInfo(res.name, res.about);
+//         cardList.forEach(data => {
+//             const newData = createCard({
+//                 name: data.name,
+//                 link: data.link,
+//                 likes: data.likes,
+//                 //id: data._id
+//             })
+//             defaultCardsList.addItem(newData)
+//         })
+//     })
+//     .catch(console.log);
+
 api.getProfile()
     .then(res => {
-        console.log('ответ', res)
         userInfo.setUserInfo(res.name, res.about)
     })
 
 api.getInitialCards()
     .then(cardList => {
         cardList.forEach(data => {
-            const newData = {
+            const newData = createCard({
                 name: data.name,
-                link: data.link
-            }
+                link: data.link,
+                likes: data.likes,
+                id: data._id
+            })
             defaultCardsList.addItem(newData)
         })
     })
 
+//валидация
 const validFormEdit = new FormValidator(validationConfig, editForm);
 const validFormaddCard = new FormValidator(validationConfig, addCardForm);
 
@@ -41,37 +58,46 @@ editOpenButton.addEventListener("click", function() {
     const userDescription = userInfo.getUserInfo();
 
     nameInput.value = userDescription.name;
-    jobInput.value = userDescription.job;
+    jobInput.value = userDescription.about;
 
     validFormEdit.resetErrorInput()
 });
 
 const editPopup = new PopupWithForm('.popup_type_edit', {
-    submitHandler: (data) => {
-        userInfo.setUserInfo(data);
-        editPopup.close();
-    }
-})
+        submitHandler: (res) => {
+            api.editProfile(res.name, res.about)
+                .then(() => {
+                    userInfo.setUserInfo(res.name, res.about);
+                    editPopup.close();
+                })
 
-//модалка добавления карточки 
+        }
+
+    })
+    //модалка добавления карточки 
 addCardButton.addEventListener("click", () => {
     addCardPopup.open();
     validFormaddCard.resetErrorInput()
 });
 
+//модалка добавления карточки 
+
 const addCardPopup = new PopupWithForm(".popup_type_add-card", {
     submitHandler: (data) => {
-        const newData = {
-            name: data.name,
-            link: data.link,
-        }
+        api.addCard(data)
+            .then(res => {
+                defaultCardsList.addItem(res)
+                const newCard = createCard(newData);
+                defaultCardsList.prependItem(newCard);
 
-        const newCard = createCard(newData);
-        defaultCardsList.prependItem(newCard); //section.addItem(card) 48.04
-
-        addCardPopup.close();
+                addCardPopup.close();
+            })
     }
 });
+
+//модалка удаления карточки
+const confirmPopup = new PopupWithForm(".popup_type_delete-confirm");
+//confirmPopup.setEventListeners()
 
 //модалка картинки
 const imgPopup = new PopupWithImage(".popup_type_img");
@@ -79,14 +105,28 @@ const imgPopup = new PopupWithImage(".popup_type_img");
 //добавление карточки
 function createCard(data) {
     const card = new Card(data, '.card-template', {
-        handleCardClick: (name, link) => {
-            imgPopup.open(name, link)
+            handleCardClick: (name, link) => {
+                imgPopup.open(name, link)
+            },
+        },
+        (id) => {
+            //console.log(id)
+            confirmPopup.open()
+            confirmPopup.cangeSubmitHandler(() => {
+                //console.log(id)
+                api.deleteCard(id)
+                    .then(res => {
+                        console.log(res)
+                        card.deleteHandler()
+                        confirmPopup.close()
+                    })
+            })
         }
-    });
+    )
     const cardElement = card.createCard();
     return cardElement // возваращаете готовую карточку
-
 }
+
 
 const defaultCardsList = new Section({
         items: [],
