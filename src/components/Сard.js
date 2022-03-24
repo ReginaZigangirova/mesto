ea43c02898d515158fad7a521c430a5009742574
Regina Zigangirova <zigangirova.reggina@yandex.ru>
2022-03-24 17:39:52 +0300

    Место 6.3

--- src/components/Сard.js
+++ src/components/Сard.js
@@ -1,27 +1,51 @@
 export class Card {
-    constructor(data, templateSelector, { handleCardClick }, handleDeleteClick) {
+    constructor(data, templateSelector, { handleCardClick }, handleDeleteClick, handleLikeClick) {
         this._name = data.name;
         this._link = data.link;
         this._likes = data.likes;
-        //this._id = data.id;
+        this._id = data.id;
+        this._userId = data.userId;
+        this._ownerId = data.ownerId;
         this._templateSelector = templateSelector;
         this._handleCardClick = handleCardClick;
         this._handleDeleteClick = handleDeleteClick;
+        this._handleLikeClick = handleLikeClick;
     }
 
     _getTemplate() {
-            const photoTemplate = document
-                .querySelector(this._templateSelector)
-                .content
-                .querySelector('.card__elements')
-                .cloneNode(true);
+        const photoTemplate = document
+            .querySelector(this._templateSelector)
+            .content
+            .querySelector('.card__elements')
+            .cloneNode(true);
 
-            return photoTemplate;
+        return photoTemplate;
+    }
+
+    isLiked() {
+            const userHasLikedCard = this._likes.find(user => user._id === this._userId)
+
+            return userHasLikedCard
         }
         //кол-во лайков 
-    _setLikes() {
-            const likeCountElement = this._element.querySelector('.card__like-count')
-            likeCountElement.textContent = this._likes.length
+    setLikes(newLikes) {
+        this._likes = newLikes
+        const likeCountElement = this._element.querySelector('.card__like-count')
+        likeCountElement.textContent = this._likes.length
+
+        //const userHasLikedCard = this._likes.find(user => user._id === this._userId)
+        if (this.isLiked()) {
+            this._addLike()
+        } else {
+            this._removeLike()
+        }
+    }
+
+    _addLike() {
+        this._likeButton.classList.add('card__like_active');
+    }
+    _removeLike() {
+            this._likeButton.classList.remove('card__like_active');
         }
         //создаем карточку 
     createCard() {
@@ -32,25 +56,26 @@ export class Card {
         this._element.querySelector('.card__tittle').textContent = this._name;
         this._likeButton = this._element.querySelector('.card__like');
         this._setEventListeners();
-        this._setLikes()
+        this.setLikes(this._likes);
+        if (this._ownerId !== this._userId) {
+            this._element.querySelector('.card__delete').style.display = 'none'
+        }
+
         return this._element;
     }
 
 
     //функция удаления карточки
-    // deleteHandler() {
-    //     this._element.querySelector('.card__delete').closest('.card__elements').remove();
-    // };
+    deleteHandler() {
+        this._element.querySelector('.card__delete').closest('.card__elements').remove();
+    };
 
-    //функция лайка
-    _handleLikeButton() {
-        this._likeButton.classList.toggle('card__like_active');
-    }
+
 
     //установка слушателей
     _setEventListeners() {
-        this._element.querySelector('.card__delete').addEventListener('click', () => { this._handleDeleteClick() });
-        this._likeButton.addEventListener('click', () => { this._handleLikeButton() });
+        this._element.querySelector('.card__delete').addEventListener('click', () => { this._handleDeleteClick(this._id) });
+        this._likeButton.addEventListener('click', () => { this._handleLikeClick(this._id) });
         this._cardImage.addEventListener('click', () => { this._handleCardClick(this._name, this._link) });
     }
 
