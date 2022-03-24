6d64d1dcbd1bfbec979f0f345991976cb4586464
Regina Zigangirova <zigangirova.reggina@yandex.ru>
2022-03-24 18:36:44 +0300

    Место 6.4

--- src/components/Api.js
+++ src/components/Api.js
@@ -2,79 +2,77 @@ class Api {
     constructor({ baseUrl, headers }) {
         this._headers = headers
         this._baseUrl = baseUrl
-            // тело конструктора
+    }
+    _checkResponse(res) {
+        if (res.ok) {
+            return res.json()
+        } else {
+            return Promise.reject(res.status)
+        }
     }
     getProfile() {
-        //console.log('getProfile')
         return fetch(`${this._baseUrl}/users/me`, {
-                headers: this._headers
-            }).then(res => res.ok ? res.json() : Promise.reject(res.status))
-            .catch(console.log)
+            headers: this._headers
+        }).then(this._checkResponse)
     }
 
     getInitialCards() {
-        //console.log('getInitialCards')
         return fetch(`${this._baseUrl}/cards`, {
-                headers: this._headers
-            }).then(res => res.ok ? res.json() : Promise.reject(res.status))
-            .catch(console.log)
+            headers: this._headers
+        }).then(this._checkResponse)
     }
     editProfile(name, about) {
         return fetch('https://mesto.nomoreparties.co/v1/cohort-37/users/me', {
-            method: 'PATCH',
-            headers: {
-                authorization: '0422eec2-f505-49eb-b34a-7793c9abd9fe',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                name,
-                about
+                method: 'PATCH',
+                headers: {
+                    authorization: '0422eec2-f505-49eb-b34a-7793c9abd9fe',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name,
+                    about
+                })
             })
-        });
+            .then(this._checkResponse)
     }
     addCard(name, link) {
         return fetch('https://mesto.nomoreparties.co/v1/cohort-37/cards', {
-                method: "POST",
-                headers: this._headers,
-                body: JSON.stringify({
-                    name,
-                    link
-                })
-            }).then(res => res.ok ? res.json() : Promise.reject(res.status))
-            .catch(console.log)
+            method: "POST",
+            headers: this._headers,
+            body: JSON.stringify({
+                name,
+                link
+            })
+        }).then(this._checkResponse)
     }
 
     deleteCard(id) {
         return fetch(`${this._baseUrl}/cards/${id} `, {
-                method: "DELETE",
-                headers: this._headers,
-            }).then(res => res.ok ? res.json() : Promise.reject(res.status))
-            .catch(console.log)
+            method: "DELETE",
+            headers: this._headers,
+        }).then(this._checkResponse)
     }
     deleteLike(id) {
         return fetch(`${this._baseUrl}/cards/${id}/likes `, {
-                method: "DELETE",
-                headers: this._headers,
-            }).then(res => res.ok ? res.json() : Promise.reject(res.status))
-            .catch(console.log)
+            method: "DELETE",
+            headers: this._headers,
+        }).then(this._checkResponse)
     }
     addLike(id) {
         return fetch(`${this._baseUrl}/cards/${id}/likes `, {
-                method: "PUT",
-                headers: this._headers,
-            }).then(res => res.ok ? res.json() : Promise.reject(res.status))
-            .catch(console.log)
+            method: "PUT",
+            headers: this._headers,
+        }).then(this._checkResponse)
     }
 
     setAvatar(avatar) {
         return fetch(`${this._baseUrl}/users/me/avatar`, {
-                method: 'PATCH',
-                headers: this._headers,
-                body: JSON.stringify({
-                    avatar
-                })
-            }).then(res => res.ok ? res.json() : Promise.reject(res.status))
-            .catch(console.log)
+            method: 'PATCH',
+            headers: this._headers,
+            body: JSON.stringify({
+                avatar
+            })
+        }).then(this._checkResponse)
     }
 }
 
